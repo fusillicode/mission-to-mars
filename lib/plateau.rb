fc53461b79a56ddae28fd4559432b871be2ea78a
fusillicode <fusillicode@gmail.com>
2016-03-20 09:34:33 +0100

    Add initial Plateau

---
+++ lib/plateau.rb
@@ -0,0 +1,56 @@
+class Plateau
+  attr_reader :top_right_coordinates, :bottom_left_coordinates
+
+  def initialize(mission_file_path)
+    @bottom_left_coordinates = extract_bottom_left_coordinates || default_bottom_left_coordinates
+    @top_right_coordinates = extract_top_right_coordinates mission_file_path
+    raise InvalidPlateau.new('Invalid plateau') unless valid?
+  end
+
+  def x_range
+    @x_range ||= (bottom_left_coordinates[0]..top_right_coordinates[0])
+  end
+
+  def y_range
+    @y_range ||= (bottom_left_coordinates[1]..top_right_coordinates[1])
+  end
+
+  def default_bottom_left_coordinates
+    Array.new dimensions_number, 0
+  end
+
+  def dimensions_number
+    2
+  end
+
+  private
+
+  def extract_bottom_left_coordinates
+    nil
+  end
+
+  def extract_top_right_coordinates(mission_file_path)
+    File.open(mission_file_path)
+        .first
+        .split(' ')
+        .tap { |coordinates| check_coordinates coordinates }
+        .map &:to_i
+  end
+
+  def check_coordinates(coordinates)
+    raise InvalidCoordinates.new('Incorrect number of coordinates') if coordinates.size != dimensions_number
+    raise InvalidCoordinates.new('No digit coordinates') if no_digit_coordinates?(coordinates)
+  end
+
+  def no_digit_coordinates?(coordinates)
+    coordinates.map { |c| c =~ /[[:alpha:]]/ }.any?
+  end
+
+  def valid?
+    top_right_coordinates[0] >= bottom_left_coordinates[0] &&
+      top_right_coordinates[1] >= bottom_left_coordinates[1]
+  end
+end
+
+class InvalidPlateau < StandardError; end
+class InvalidCoordinates < StandardError; end
---
+++ spec/plateau_spec.rb
@@ -0,0 +1,91 @@
+require 'rspec'
+require 'spec_helper'
+require 'plateau'
+
+describe Plateau do
+  describe '#initialize' do
+    it { expect { described_class.new }.to raise_error ArgumentError }
+
+    it do
+      not_readable_mission = 'spec/fixtures/not_readable_mission'
+      FileUtils.chmod 'a-r', not_readable_mission
+      expect { described_class.new(not_readable_mission) }
+        .to raise_error(Errno::EACCES)
+        .with_message /Permission denied/
+      FileUtils.chmod 'a+r', not_readable_mission
+    end
+
+    it do
+      expect { described_class.new('inexistent_file') }
+        .to raise_error(Errno::ENOENT)
+        .with_message /No such file or directory/
+    end
+
+    it do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['1 2 3'])
+      expect { described_class.new('dummy_file_path') }
+        .to raise_error(InvalidCoordinates)
+        .with_message /Incorrect number of coordinates/
+    end
+
+    it do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['N 3'])
+      expect { described_class.new('dummy_file_path') }
+        .to raise_error(InvalidCoordinates)
+        .with_message /No digit coordinates/
+    end
+
+    it do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['-1 -1'])
+      expect { described_class.new('dummy_file_path') }
+        .to raise_error(InvalidPlateau)
+        .with_message /Invalid plateau/
+    end
+
+    context 'when everything is fine' do
+      let(:subject) { described_class.new('dummy_file_path') }
+      before do
+        allow(File).to receive(:open).with('dummy_file_path').and_return(['2 3'])
+      end
+
+      it do
+        expect { described_class.new('dummy_file_path') }.not_to raise_error
+      end
+
+      it 'sets the bottom_left_coordinates' do
+        expect(subject.bottom_left_coordinates).to eq [0, 0]
+      end
+
+      it 'sets the top_right_coordinates' do
+        expect(subject.top_right_coordinates).to eq [2, 3]
+      end
+    end
+  end
+
+  describe '#default_bottom_left_coordinates' do
+    let(:subject) do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['2 3'])
+      described_class.new('dummy_file_path')
+    end
+
+    it { expect(subject.default_bottom_left_coordinates).to eq [0, 0] }
+  end
+
+  describe '#x_range' do
+    let(:subject) do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['2 3'])
+      described_class.new('dummy_file_path')
+    end
+
+    it { expect(subject.x_range).to eq (0..2) }
+  end
+
+  describe '#y_range' do
+    let(:subject) do
+      allow(File).to receive(:open).with('dummy_file_path').and_return(['2 3'])
+      described_class.new('dummy_file_path')
+    end
+
+    it { expect(subject.y_range).to eq (0..3) }
+  end
+end
